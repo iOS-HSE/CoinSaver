2b52d86f993937a1762a187bf9a615f665a35dbf
fiskirton <fiskirton@mail.com>
2020-12-11 14:17:13 +0300

    working on goals board

--- CoinSaver/Views/GoalsViewController.swift
+++ CoinSaver/Views/GoalsViewController.swift
@@ -7,11 +7,27 @@
 
 import UIKit
 
-class GoalsViewController: TabItemViewController {
+class GoalsViewController: TabItemViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Goals"
+        
+        self.addButton.initFloatingButton()
     }
 
 }
---
+++ Pods/Target Support Files/Pods-CoinSaver-CoinSaverUITests/Pods-CoinSaver-CoinSaverUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoinSaver_CoinSaverUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CoinSaver_CoinSaverUITests
+@end
