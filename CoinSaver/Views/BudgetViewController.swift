c5b145659f035042400d6af8a31f8f190974f362
geniusserg <shyundaid@gmail.com>
2020-12-13 18:23:45 +0000

    language changes; setup collection view

--- CoinSaver/Views/BudgetViewController.swift
+++ CoinSaver/Views/BudgetViewController.swift
@@ -6,12 +6,74 @@
 //
 
 import UIKit
-
+struct BudgetCategory{
+    var image: UIImage
+    var type: String
+}
 class BudgetViewController: TabItemViewController {
-
+    ///TODO: load images
+    var imageArray = [ UIImage(named: "first"),
+                       UIImage(named: "first"),
+                       UIImage(named: "first"),
+                       UIImage(named: "first"),
+                       UIImage(named: "first")
+                       
+    ]
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var topUpSumInput: UITextField!
+    @IBAction func topUpAction(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Budget"
     }
 
+    func didSetCategory(category : BudgetCategory){
+        
+    }
+    
+    func getCategories() -> [BudgetCategory]{
+        return []
+    }
+    
+    func didChangeBalance(balance: Int) -> Bool{
+        return false
+    }
+    
+    func topUpBalance(sum : Int) -> Bool{
+        return false
+    }
+    
+    func didChangePrice(sum: Int) -> Bool{
+        return false
+    }
+    
+    func makeSpending(category: BudgetCategory, sum: Int) -> Bool{
+        return false
+    }
+    
+    
 }
+
+extension BudgetViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        imageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BudgetCell", for: indexPath) as! CategoryCollectionViewCell
+        cell.image.image = imageArray[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/3, height: collectionView.frame.height/3)
+    }
+       
+    
+}
+
---
+++ CoinSaver/Views/CategoryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCollectionViewCell.swift
+//  CoinSaver
+//
+//  Created by administrator on 13/12/2020.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+    
+}
